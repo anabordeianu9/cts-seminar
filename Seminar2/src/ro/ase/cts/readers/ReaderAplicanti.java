fb82606c64da1c62b3e9440a71500d3da4721e30
Ana-Maria Bordeianu <anamariabordeianu9@gmail.com>
2021-03-13 14:28:33 +0200

    update readere

--- Seminar2/src/ro/ase/cts/readers/ReaderAplicanti.java
+++ Seminar2/src/ro/ase/cts/readers/ReaderAplicanti.java
@@ -1,7 +1,8 @@
-package ro.ase.cts.clase.readers;
+package src.ro.ase.cts.readers;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import ro.ase.cts.clase.Aplicant;
 
@@ -12,10 +13,23 @@ public abstract class ReaderAplicanti {
 		super();
 		this.fileName = fileName;
 	}
-		public abstract List<Aplicant>readAplicanti(String fileName) throws FileNotFoundException;
-		public List<Aplicant> readAplicanti() throws FileNotFoundException {
-			// TODO Auto-generated method stub
-			return null;
-		}
+		public abstract List<src.ro.ase.cts.clase.Aplicant>readAplicanti(String fileName) throws FileNotFoundException;
+		public abstract List<src.ro.ase.cts.clase.Aplicant> readAplicanti() throws FileNotFoundException;
       
+		public void citesteAplicant (Scanner input, src.ro.ase.cts.clase.Aplicant aplicant) {
+			String nume = input.next();
+			String prenume = input.next();
+			int varsta = input.nextInt();
+			int punctaj = input.nextInt();
+			int nr = input.nextInt();
+			String[] vect = new String[5];
+			for (int i = 0; i < nr; i++)
+				vect[i] = input.next();
+			aplicant.setNume(nume);
+			aplicant.setPrenume(prenume);
+			aplicant.setVarsta(varsta);
+			aplicant.setPunctaj(punctaj);
+			aplicant.setNr_proiecte(nr);
+			aplicant.setDenumireProiect(vect, nr);
+		}
 }
